724157b9510dd289fc6c627cd915923f704908a0
Juliana Guzmán <juliguzmac1906@gmail.com>
2023-09-20 17:56:05 -0500

    Se crean las funciones 1,2,3,5, se hacen ajustes en el html y se crean los dos eventos aún incompletos.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,37 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const numPalabras = text.trim().split(' ').filter(word => word !== '')
+    return numPalabras.length
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const longitudCaracterSinEspacios = text.replace(/[^\w\s]/g, '') 
+    return longitudCaracterSinEspacios.length
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let conteoNum = 0
+
+    for(let i = 0; i < text.length; i++) {
+      const caracter = text[i]
+      const parseNum = parseInt(caracter)
+
+      if(!isNaN(parseNum)) {
+        conteoNum++
+      }
+    } return conteoNum
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const userInput = document.querySelector('textarea[name="user-input"]');
+const wordCountElement = document.querySelector('li.metric:nth-child(1)');
+const charCountElement = document.querySelector('li.metric:nth-child(2)');
+const charCountNoSpacesElement = document.querySelector('li.metric:nth-child(3)');
+const numberCountElement = document.querySelector('li.metric:nth-child(4)');
+const totalNumberSumElement = document.querySelector('li.metric:nth-child(5)');
+const wordLengthAverageElement = document.querySelector('li.metric:nth-child(6)');
+const resetButton = document.querySelector('#reset-button');
+
+userInput.addEventListener ('input', () => {
+  const text = userInput.value
+
+  const wordCount = analyzer.getWordCount(text)
+  const charCount = analyzer.getCharacterCount(text)
+  const numberCount = analyzer.getNumberCount(text)
+  const charCountNoSpaces = analyzer.getCharacterCountExcludingSpaces(text)
+  const totalNumberSum = analyzer.getNumberCount(text)
+
+  wordCountElement.textContent = `Recuento de palabras: ${wordCount}`
+  charCountElement.textContent = `Recuento de caracteres: ${charCount}`
+  numberCountElement.textContent = `Recuento de numeros: ${numberCount}`
+  charCountNoSpacesElement.textContent = `Recuento caracteres sin espacios y signos de puntuación: ${charCountNoSpaces}`
+  totalNumberSumElement.textContent = `Suma total de numeros: ${totalNumberSum}`
+  wordLengthAverageElement.textContent = `Longitud media de las palabras: ${totalNumberSum}`
+
+})
+
+
+resetButton.addEventListener ('click', () => {
+  wordCountElement.textContent = "Recuento de palabras:"
+  charCountElement.textContent = "Recuento de caracteres:"
+  numberCountElement.textContent = "Recuento de numeros:"
+  charCountNoSpacesElement.textContent = "Recuento caracteres sin espacios y signos de puntuación:"
+  userInput.textContent = ""
+})
+
